3f336138a01dae1ba293349c8a7791279e042bd3
dzs2311 <2856841640@qq.com>
2023-01-24 17:02:15 +0800

    UU UW UI Icons&Models Add

---
+++ Super Power - Remaking of World Order (v 7)/Gameplay/SQL/ArtDefines/ArtDefines_Improvements.sql
@@ -0,0 +1,22 @@
+--==========================================================================================================================
+-- BuildingClasses
+--==========================================================================================================================	
+INSERT INTO ArtDefine_LandmarkTypes(Type, 	         LandmarkType, 	FriendlyName)
+SELECT 'ART_DEF_IMPROVEMENT_SHOSHONE_BEASTDOME', 	'Improvement', 	'SHOSHONE_BEASTDOME';
+--==========================================================================================================================
+-- BuildingClasses
+--==========================================================================================================================	
+INSERT INTO ArtDefine_Landmarks
+		(Era, 	State, 					Scale,	ImprovementType,						    LayoutHandler, 	ResourceType, 			Model, 										TerrainContour)
+VALUES 	('Any', 'UnderConstruction', 	1.0,  	'ART_DEF_IMPROVEMENT_SHOSHONE_BEASTDOME',   'SNAPSHOT', 	'ART_DEF_RESOURCE_ALL', 'North American Encampment 1 HB.fxsxml', 	1),
+		('Any', 'Constructed',		 	1.0, 	'ART_DEF_IMPROVEMENT_SHOSHONE_BEASTDOME',   'SNAPSHOT', 	'ART_DEF_RESOURCE_ALL', 'North American Encampment 1 B.fxsxml', 	1),
+		('Any', 'Pillaged',				1.0,  	'ART_DEF_IMPROVEMENT_SHOSHONE_BEASTDOME',   'SNAPSHOT', 	'ART_DEF_RESOURCE_ALL', 'North American Encampment 1 PL.fxsxml', 	1);
+
+--==========================================================================================================================
+-- ArtDefine_StrategicView
+--==========================================================================================================================
+INSERT INTO ArtDefine_StrategicView
+            (StrategicViewType,								TileType,		Asset)
+VALUES      ('ART_DEF_IMPROVEMENT_SHOSHONE_BEASTDOME',		'Improvement',	'SV_BeastDome.dds');
+--==========================================================================================================================
+--==========================================================================================================================
