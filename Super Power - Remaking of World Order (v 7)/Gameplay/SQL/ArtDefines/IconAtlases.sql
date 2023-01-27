ae918fec32f8cfb5b37ff995ac5880ee1b1b8a00
dzs2311 <2856841640@qq.com>
2023-01-27 11:38:35 +0800

    Germany UU:TigerHeli Remove and Dopplesoldners add

--- Super Power - Remaking of World Order (v 7)/Gameplay/SQL/ArtDefines/IconAtlases.sql
+++ Super Power - Remaking of World Order (v 7)/Gameplay/SQL/ArtDefines/IconAtlases.sql
@@ -10,18 +10,11 @@ VALUES		('SP8_EXTRA_ATLAS', 				256, 		'SP8Units&BuildingsAtlas256.dds',				4, 	
             ('SP8_EXTRA_ATLAS', 				048, 		'SP8Units&BuildingsAtlas048.dds',				4, 				1),
 			('SP8_EXTRA_ATLAS', 				045, 		'SP8Units&BuildingsAtlas045.dds',				4, 				1),
 			('SP8_EXTRA_ATLAS', 				024, 		'SP8Units&BuildingsAtlas024.dds',				4, 				1),
-			('SP8_EXTRA_ATLAS', 				032, 		'SP8Units&BuildingsAtlas032.dds',				4, 				1);
--- ARTDEFINES
---==========================================================================================================================	
-
-------------------------------
--- IconTextureAtlases
-------------------------------
-INSERT INTO IconTextureAtlases 
-		(Atlas, 						IconSize, 	Filename, 								IconsPerRow, 	IconsPerColumn)
-VALUES	('BUILDING_AZTEC_ATLAS', 		256, 		'HueyTeocalliPicture_256.dds',			1, 				1),
-		('BUILDING_AZTEC_ATLAS', 		128, 		'HueyTeocalliPicture_128.dds',			1, 				1),
-		('BUILDING_AZTEC_ATLAS', 		45, 		'HueyTeocalliPicture_045.dds',			1, 				1),
-		('BUILDING_AZTEC_ATLAS', 		64, 		'HueyTeocalliPicture_064.dds',			1, 				1);
+			('SP8_EXTRA_ATLAS', 				032, 		'SP8Units&BuildingsAtlas032.dds',				4, 				1),
+			('UNITS_GERMAN_DOPPE_FLAG_ATLAS',	032,		'SV_Doppe.dds',									1,				1),
+			('BUILDING_AZTEC_ATLAS', 			256, 		'HueyTeocalliPicture_256.dds',					1, 				1),
+			('BUILDING_AZTEC_ATLAS', 			128, 		'HueyTeocalliPicture_128.dds',					1, 				1),
+			('BUILDING_AZTEC_ATLAS', 			045, 		'HueyTeocalliPicture_045.dds',					1, 				1),
+			('BUILDING_AZTEC_ATLAS', 			064, 		'HueyTeocalliPicture_064.dds',					1, 				1);
 --==========================================================================================================================
 --==========================================================================================================================
