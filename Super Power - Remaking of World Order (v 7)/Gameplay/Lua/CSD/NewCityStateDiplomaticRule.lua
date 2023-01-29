c1b7912286260b77058573407efb39306dc2ad4c
llyyaacn <llyyaacn@163.com>
2023-01-29 21:30:32 +0800

    Block command "gift" when pregame settings disabled giving militia units to city states & Update DLL

--- Super Power - Remaking of World Order (v 7)/Gameplay/Lua/CSD/NewCityStateDiplomaticRule.lua
+++ Super Power - Remaking of World Order (v 7)/Gameplay/Lua/CSD/NewCityStateDiplomaticRule.lua
@@ -16,4 +16,31 @@ function AbleGiftMilitia(iMajor, iMinor, iUnit)
 end
 GameEvents.PlayerCanGiftUnit.Add(AbleGiftMilitia);
 
+function BlockMilitiaGiving(iPlayer, iUnit, iCommand, iData1, iData2, iPlotX, iPlotY, bTestVisible)
+    if not (iCommand == CommandTypes.COMMAND_GIFT) then 
+        return true 
+    end
+    local enableMilitiaGiving = PreGame.GetGameOption("GAMEOPTION_ABLE_GIFT_MILITIA") == 1
+    local pPlot = Map.GetPlot(iPlotX, iPlotY)
+    local pPlayer = Players[iPlayer]
+    if (not pPlayer) or (not pPlot) then 
+        --Yield condition check
+        return true
+    end
+
+    local pUnit = pPlayer:GetUnitByID(iUnit)
+    if pUnit and pPlot:IsOwned() then
+        local pTargetPlayer = Players[pPlot:GetOwner()]
+        if pTargetPlayer:IsMinorCiv() then
+            return (not pUnit:IsHasPromotion(GameInfoTypes.PROMOTION_MILITIA_COMBAT)) or enableMilitiaGiving
+        else
+            return true
+        end
+    else
+        --Yield condition check
+        return true
+    end
+end
+GameEvents.CanDoCommand.Add(BlockMilitiaGiving);
+
 print("NewCityStateDiplomaticRule: Check Pass!");
