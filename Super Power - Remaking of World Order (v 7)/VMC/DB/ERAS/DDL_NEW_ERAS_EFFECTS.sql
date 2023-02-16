e4cc829adbe269280cd183e1a6c0376ccedc8a22
qiuhuiming <1985849912zz@gmail.com>
2023-02-16 22:54:53 +0800

    feature: Polynesia can found cities on coast plot

--- Super Power - Remaking of World Order (v 7)/VMC/DB/ERAS/DDL_NEW_ERAS_EFFECTS.sql
+++ Super Power - Remaking of World Order (v 7)/VMC/DB/ERAS/DDL_NEW_ERAS_EFFECTS.sql
@@ -3,3 +3,8 @@ create table Era_MountainCityYieldChanges (
 	YieldType text not null REFERENCES Yields(Type),
 	Yield integer not null
 );
+create table Era_CoastCityYieldChanges (
+	EraType text not null REFERENCES Eras(Type),
+	YieldType text not null REFERENCES Yields(Type),
+	Yield integer not null
+);
--- Super Power - Remaking of World Order (v 7)/VMC/DB/TRAITS/DDL_Traits.sql
+++ Super Power - Remaking of World Order (v 7)/VMC/DB/TRAITS/DDL_Traits.sql
@@ -4,3 +4,4 @@ alter table Traits add column `AdequateLuxuryCompleteQuestInfluenceModifierMax` 
 alter table Traits add column `AllyCityStateCombatModifier` int not null default 0;
 alter table Traits add column `AllyCityStateCombatModifierMax` int not null default -1;
 alter table Traits add column `CanFoundMountainCity` boolean not null default 0;
+alter table Traits add column `CanFoundCoastCity` boolean not null default 0;
