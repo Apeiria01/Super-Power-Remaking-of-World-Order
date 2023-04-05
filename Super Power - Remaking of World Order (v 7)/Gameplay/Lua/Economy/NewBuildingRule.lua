0440d02effa4966fb01e453e5ba2925f503bcd40
qingyin3545 <3138584199@qq.com>
2023-04-05 16:17:21 +0800

    bugifx

--- Super Power - Remaking of World Order (v 7)/Gameplay/Lua/Economy/NewBuildingRule.lua
+++ Super Power - Remaking of World Order (v 7)/Gameplay/Lua/Economy/NewBuildingRule.lua
@@ -18,20 +18,33 @@ function NromanCampBouns(iPlayer)
     local eEra = pPlayer:GetCurrentEra()
     local iNromanCampBonus = (eEra + 1) * NumOfNromanCamp
     print("NormanCamp Random",NromanCampRandom,iNromanCampBonus)
-    local hex
-    for pCity in pPlayer:Cities() do
-        if pCity:IsHasBuilding(GameInfoTypes.BUILDING_NORMAN_CAMP) then
-            hex = ToHexFromGrid(Vector2(pCity:GetX(),pCity:GetY()))
-            if NromanCampRandom < 2 then
-                pCity:SetOverflowProduction(pCity:GetOverflowProduction() + iNromanCampBonus)
-                Events.AddPopupTextEvent(HexToWorld(hex), Locale.ConvertTextKey("+{1_Num}[ICON_PRODUCTION]",iNromanCampBonus))
-            else
-                pCity:ChangeFood(iNromanCampBonus)
-                Events.AddPopupTextEvent(HexToWorld(hex), Locale.ConvertTextKey("+{1_Num}[ICON_FOOD]",iNromanCampBonus))
+    if pPlayer:IsHuman() then
+        local hex
+        for pCity in pPlayer:Cities() do
+            if pCity:IsHasBuilding(GameInfoTypes.BUILDING_NORMAN_CAMP) then
+                hex = ToHexFromGrid(Vector2(pCity:GetX(),pCity:GetY()))
+                if NromanCampRandom < 2 then
+                    pCity:SetOverflowProduction(pCity:GetOverflowProduction() + iNromanCampBonus)
+                    Events.AddPopupTextEvent(HexToWorld(hex), Locale.ConvertTextKey("+{1_Num}[ICON_PRODUCTION]",iNromanCampBonus))
+                else
+                    pCity:ChangeFood(iNromanCampBonus)
+                    Events.AddPopupTextEvent(HexToWorld(hex), Locale.ConvertTextKey("+{1_Num}[ICON_FOOD]",iNromanCampBonus))
+                end
+                Events.GameplayFX(hex.x, hex.y, -1)
             end
-            Events.GameplayFX(hex.x, hex.y, -1)
+        end
+    else
+        for pCity in pPlayer:Cities() do
+            if pCity:IsHasBuilding(GameInfoTypes.BUILDING_NORMAN_CAMP) then
+                if NromanCampRandom < 2 then
+                    pCity:SetOverflowProduction(pCity:GetOverflowProduction() + iNromanCampBonus)
+                else
+                    pCity:ChangeFood(iNromanCampBonus)
+                end
+            end
         end
     end
+    
 end
 
 function SPNNromanCampConquestedCity(oldOwnerID, isCapital, cityX, cityY, newOwnerID, numPop, isConquest)
