24c73161563acc343f525e3738230202b1308ce7
qiuhuiming <1985849912zz@gmail.com>
2023-04-28 20:00:28 +0800

    refactor: split DLL

--- Super Power - Remaking of World Order (v 7)/VMC/DB/PROMOTIONS/CapitalLand.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE UnitPromotions ADD 'OnCapitalLandAttackMod' INTEGER DEFAULT 0;
-ALTER TABLE UnitPromotions ADD 'OutsideCapitalLandAttackMod' INTEGER DEFAULT 0;
-ALTER TABLE UnitPromotions ADD 'OnCapitalLandDefenseMod' INTEGER DEFAULT 0;
-ALTER TABLE UnitPromotions ADD 'OutsideCapitalLandDefenseMod' INTEGER DEFAULT 0;
