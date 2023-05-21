95a992b348ffbd6dc96da7cc42abdbf5ebba916a
qiuhuiming <1985849912zz@gmail.com>
2023-05-21 13:50:33 +0800

    fix: change counter attack field name

--- Super Power - Remaking of World Order (v 7)/Gameplay/SQL/Units/Unit_StackingCounterAttack.sql
+++ Super Power - Remaking of World Order (v 7)/Gameplay/SQL/Units/Unit_StackingCounterAttack.sql
@@ -1,4 +1,4 @@
 -- PROMOTION_COLLECTION_ARCHERY_COMBAT counter attack
 insert into PromotionCollections(Type, StackingFightBack) values ('PROMOTION_COLLECTION_ARCHERY_COMBAT', 1);
-insert into PromotionCollections_Entries(CollectionType, PromotionIndex, PromotionType, StackingFightBackOnlyMelee) values
+insert into PromotionCollections_Entries(CollectionType, PromotionIndex, PromotionType, TriggerMeleeDefense) values
 ('PROMOTION_COLLECTION_ARCHERY_COMBAT', 1, 'PROMOTION_ARCHERY_COMBAT', 1);
