31688c66303f203c19f1486e2ac15e6fa2826886
qingyin3545 <3138584199@qq.com>
2023-11-09 15:42:20 +0800

    fix: GoodyHuts

--- Super Power - Remaking of World Order (v 7)/Gameplay/SQL/Terrian/HandicapInfo_Goodies.sql
+++ Super Power - Remaking of World Order (v 7)/Gameplay/SQL/Terrian/HandicapInfo_Goodies.sql
@@ -1,7 +1,7 @@
 INSERT INTO HandicapInfo_Goodies SELECT HandicapInfos.Type, GoodyHuts.Type FROM HandicapInfos, GoodyHuts;
 
 CREATE TABLE IDRemapper ( id INTEGER PRIMARY KEY AUTOINCREMENT, Type TEXT );
-INSERT INTO IDRemapper (Type) SELECT Type FROM GoodyHuts;
+INSERT INTO IDRemapper (Type) SELECT Type FROM GoodyHuts ORDER BY ID ASC;
 UPDATE GoodyHuts SET ID =	( SELECT IDRemapper.id-1 FROM IDRemapper WHERE GoodyHuts.Type = IDRemapper.Type);
 
 DROP TABLE IDRemapper;
