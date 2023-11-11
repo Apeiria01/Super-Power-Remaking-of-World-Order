0c0df370e4536c794b7f26e554140d54174b3187
qiuhuiming <1985849912zz@gmail.com>
2023-11-11 17:58:01 +0800

    update Sweden UA

--- Super Power - Remaking of World Order (v 7)/UI/InGame/TopPanel.lua
+++ Super Power - Remaking of World Order (v 7)/UI/InGame/TopPanel.lua
@@ -511,7 +511,24 @@ function ScienceTipHandler( control )
 			strText = strText .. Locale.ConvertTextKey("TXT_KEY_TP_SCIENCE_FROM_CITIES", iScienceFromCities / 100);
 			strText = strText .. "[NEWLINE]";
 		end
-	
+
+    local tScienceTimes100FromFriendsTable= pPlayer:GetScienceTimes100FromFriendsTable();
+    -- for each row in the table, ConvertTextKey args is index and value
+    local bFirstEntryScienceFromFriends = true
+		for playerID = 0, GameDefines.MAX_MAJOR_CIVS - 1 do
+      local value = tScienceTimes100FromFriendsTable[playerID]
+      local player = Players[playerID]
+      if value ~= nil and value ~= 0 and player ~= nil then
+          if bFirstEntryScienceFromFriends then
+              strText = strText .. Locale.ConvertTextKey("TXT_KEY_TP_TITLE_SCIENCE_FROM_FRIENDS")
+              bFirstEntryScienceFromFriends = false
+          end
+
+          local civName = player:GetCivilizationShortDescription()
+          strText = strText .. Locale.ConvertTextKey("TXT_KEY_TP_SCIENCE_FROM_FRIENDS", civName, value / 100.0)
+      end
+    end
+
 		-- Science from Trade Routes
 		local iScienceFromTrade = pPlayer:GetAllTradeValue(GameInfoTypes["YIELD_SCIENCE"])
 		if (iScienceFromTrade ~= 0) then
