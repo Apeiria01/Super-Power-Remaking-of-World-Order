d0e31fac57eedd55f7e6556f3cdbff15f6b90e2b
qingyin3545 <3138584199@qq.com>
2023-11-19 12:08:25 +0800

    fix: PROMOTION_FORMATION Upgrade

--- Super Power - Remaking of World Order (v 7)/Gameplay/SQL/Units/Units_Land_Infantry.sql
+++ Super Power - Remaking of World Order (v 7)/Gameplay/SQL/Units/Units_Land_Infantry.sql
@@ -2,3 +2,7 @@ insert into Unit_FreePromotions (UnitType, PromotionType)
 select `Type`, 'PROMOTION_ANTI_RIOT_BONUS'
 from Units 
 where Class = 'UNITCLASS_MARINE' or Class = 'UNITCLASS_PARATROOPER' or Class = 'UNITCLASS_XCOM_SQUAD';
+
+INSERT INTO UnitPromotions_PromotionUpgrade(PromotionType, JudgePromotionType, NewPromotionType)
+SELECT 'PROMOTION_FORMATION_1', 'PROMOTION_ANTI_TANK', 'PROMOTION_AMBUSH_1' UNION ALL
+SELECT 'PROMOTION_FORMATION_2', 'PROMOTION_ANTI_TANK', 'PROMOTION_AMBUSH_2';
