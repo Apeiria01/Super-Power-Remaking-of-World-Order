b8c1f39ce2ccab44143368c18ccf6555d42ee1bf
qingyin3545 <3138584199@qq.com>
2023-12-16 14:20:26 +0800

    apply: AttackBonusAdjacentWhenUnitKilled

--- Super Power - Remaking of World Order (v 7)/UI/InGame/WorldView/EnemyUnitPanel.lua
+++ Super Power - Remaking of World Order (v 7)/UI/InGame/WorldView/EnemyUnitPanel.lua
@@ -635,6 +635,14 @@ function UpdateCombatOddsUnitVsCity(pMyUnit, pCity)
 				controlTable.Value:SetText( GetFormattedText(strText, iModifier, true, true) );
 			end
 
+			--Bonus from Death Unit
+			iModifier = pMyUnit:GetAttackBonusFromDeathUnit();
+			if (iModifier ~= 0) then
+				controlTable = g_MyCombatDataIM:GetInstance();
+				controlTable.Text:LocalizeAndSetText( "TXT_KEY_EUPANEL_DEATH_ATTACK_MODIFIER" );
+				controlTable.Value:SetText( GetFormattedText(strText, iModifier, true, true) );
+			end
+
 			-- City Attack bonus
 			local iModifier = pMyUnit:CityAttackModifier();
 			if (iModifier ~= 0) then
@@ -1368,6 +1376,14 @@ function UpdateCombatOddsUnitVsUnit(pMyUnit, pTheirUnit)
 					controlTable.Text:LocalizeAndSetText("TXT_KEY_EUPANEL_ENEMY_CITY_BELIEF_BONUS");
 					controlTable.Value:SetText(GetFormattedText(strText, iModifier, true, true));
 				end
+			end
+
+			--Bonus from Death Unit
+			iModifier = pMyUnit:GetAttackBonusFromDeathUnit();
+			if (iModifier ~= 0) then
+				controlTable = g_MyCombatDataIM:GetInstance();
+				controlTable.Text:LocalizeAndSetText( "TXT_KEY_EUPANEL_DEATH_ATTACK_MODIFIER" );
+				controlTable.Value:SetText( GetFormattedText(strText, iModifier, true, true) );
 			end
 
 			-- Empire Unhappy
