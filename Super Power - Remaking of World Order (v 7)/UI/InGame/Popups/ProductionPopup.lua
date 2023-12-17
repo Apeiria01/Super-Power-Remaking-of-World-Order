7f7d109874d1c2bf7b372b47203d6aa00b4d0fca
qingyin3545 <3138584199@qq.com>
2023-12-17 22:21:12 +0800

    GetHelpTextForProject() and Huey Teocalli fix

--- Super Power - Remaking of World Order (v 7)/UI/InGame/Popups/ProductionPopup.lua
+++ Super Power - Remaking of World Order (v 7)/UI/InGame/Popups/ProductionPopup.lua
@@ -843,7 +843,7 @@ function UpdateWindow( city )
 			Controls.ProductionPortrait:SetHide( false );
 			
 			-- Info for this thing
-			strToolTip = Locale.ConvertTextKey(GetHelpTextForProject(projectProduction, true)) .. "[NEWLINE][NEWLINE]" .. strToolTip;
+			strToolTip = Locale.ConvertTextKey(GetHelpTextForProject(projectProduction, true, city)) .. "[NEWLINE][NEWLINE]" .. strToolTip;
 			
 		else
 			Controls.ProductionPortrait:SetHide( true );
@@ -1295,7 +1295,7 @@ function AddProductionButton( id, description, orderType, turnsLeft, column, isD
 				strToolTip = GetHelpTextForBuilding(id, bExcludeName, bExcludeHeader, false, pCity);
 			else
 				local bIncludeRequirementsInfo = false;
-				strToolTip = GetHelpTextForProject(id, bIncludeRequirementsInfo);
+				strToolTip = GetHelpTextForProject(id, bIncludeRequirementsInfo, pCity);
 			end
 			
 			-- Disabled help text
