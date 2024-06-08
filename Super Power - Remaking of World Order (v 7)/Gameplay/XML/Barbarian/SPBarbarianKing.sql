f09d7110202d2a54bd7aec6962d9757debb1385f
catgrep <1985849912zz@gmail.com>
2024-06-08 16:27:11 +0800

    tmp

--- Super Power - Remaking of World Order (v 7)/Gameplay/XML/Barbarian/SPBarbarianKing.sql
+++ Super Power - Remaking of World Order (v 7)/Gameplay/XML/Barbarian/SPBarbarianKing.sql
@@ -1,15 +1,15 @@
 insert into BarbarianKings
-(UnitType,                          LandWeight, SeaWeight) values
-('UNIT_POLISH_PZLW3_HELICOPTER',    100,        50),
-('UNIT_ENGLISH_CROMWELL_TANK',      50,         50),
-('UNIT_ENGLISH_CROMWELL_TANK',      50,         50),
-('UNIT_CHINESE_052D',               0,          400),
-('UNIT_JAPANESE_DESTROYER',         0,          400);
+(UnitType,                          LandWeight, SeaWeight, MaxInstance) values
+('UNIT_POLISH_PZLW3_HELICOPTER',    100,        50,        1),
+('UNIT_ENGLISH_CROMWELL_TANK',      50,         50,        1),
+('UNIT_ENGLISH_CROMWELL_TANK',      50,         50,        1),
+('UNIT_CHINESE_052D',               0,          400,       1),
+('UNIT_JAPANESE_DESTROYER',         0,          400,       1),
+('UNIT_MECH',                       1,          1,         1000);
 
-insert into BarbarianFreeBuildings
-(BuildingType) values
-('BUILDING_WALLS'),
-('BUILDING_BARRACKS'),
-('UNIT_ENGLISH_CROMWELL_TANK'),
-('UNIT_CHINESE_052D'),
-('UNIT_JAPANESE_DESTROYER');
+insert into BarbarianCityFreeBuildings
+(BuildingType, Num) values
+('BUILDING_WALLS', 1),
+('BUILDING_BARRACKS', 1);
+
+update DEFINES set Value = 100 where Name = 'BARBARIAN_CITY_SPAWN_PROBABLITY';
