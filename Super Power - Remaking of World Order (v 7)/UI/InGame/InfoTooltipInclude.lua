d414cb6f21f199cb5dc3934e7c5c0f85062f4fe2
LoveEmperor <2845798568@qq.com>
2025-03-21 10:33:21 +0800

    Building_YieldModifiersChangesPerEra

--- Super Power - Remaking of World Order (v 7)/UI/InGame/InfoTooltipInclude.lua
+++ Super Power - Remaking of World Order (v 7)/UI/InGame/InfoTooltipInclude.lua
@@ -1846,6 +1846,14 @@ function GetHelpTextForBuilding( buildingID, bExcludeName, bExcludeHeader, bNoMa
 				( " +" .. value .. L(yieldInfo.IconString) ..L("TXT_KEY_TRADE_BONUS_PER_ERA")))
 		end
 	end
+	for row in GameInfo.Building_YieldModifiersChangesPerEra(thisBuildingType) do
+		local yieldInfo = GameInfo.Yields[row.YieldType]
+		local value = row.Yield or 0
+		if yieldInfo and (value or 0) > 0 then
+			insert(tips,
+				( " +" .. value .."%" ..L(yieldInfo.IconString) ..L("TXT_KEY_Building_PER_ERA")))
+		end
+	end
 
 	-- Yields enhanced by Technology
 	if techFilter( enhancedYieldTech ) then
